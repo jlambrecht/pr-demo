e755848d66d64cc8132abc581c4506a7c40f0d71
Jacob Lambrecht <jdlambrecht@gmail.com>
2017-05-04 21:18:49 -0700

    backend and frontend are working now

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,14 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.updateResult = functions.database.ref('/form-submissions/{uname}')
+  .onWrite(event => {
+    const original = event.data.val();
+    if (original) {
+      var result = "Hello " + original.uname + " from Google Cloud Functions! Your age has been recorded as " + original.age + ".";
+      return event.data.ref.child('result').set(result);
+    }
+    return false;
+  });
+
